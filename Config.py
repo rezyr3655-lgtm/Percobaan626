9bb8f53ca54009c7a98c9b527d30e55de1b0e779
rezyr3655-lgtm <rezyr3655@gmail.com>
2026-07-01 12:34:39 +0700

    Add initial configuration for XNX THEME

---
+++ Config.py
@@ -0,0 +1,17 @@
+# ==============================
+# XNX THEME CONFIG
+# ==============================
+
+TOOL_NAME = "XNX THEME"
+VERSION = "1.0"
+AUTHOR = "xnx jakarta"
+
+COLOR = {
+    "red": "\033[91m",
+    "green": "\033[92m",
+    "yellow": "\033[93m",
+    "blue": "\033[94m",
+    "cyan": "\033[96m",
+    "white": "\033[97m",
+    "reset": "\033[0m"
+}
